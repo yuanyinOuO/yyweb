2eae5d638d828814e9ad847b33398cf413684b5a
yuanyinOuO <yuanyinOuO@gmail.com>
2022-06-28 14:15:58 +0800

    2022/6/28-2.15

--- javascript.js
+++ javascript.js
@@ -2,3 +2,22 @@ function toggleMenu(number){
     var menu=document.getElementById("menu-"+number);
     menu.classList.toggle("hide");
 }
+
+
+
+var home = document.getElementById("home"),
+    note = document.getElementById("note"),
+    live2dtalk = document.getElementById("live2dtalk")
+
+home.addEventListener("mouseover" , function(){
+    live2dtalk.innerText = "回到主頁面"
+})
+home.addEventListener("mouseout" , function(){
+    live2dtalk.innerText = "welcome!"
+})
+note.addEventListener("mouseover" , function(){
+    live2dtalk.innerText = "這個是筆記連結!!"
+})
+note.addEventListener("mouseout" , function(){
+    live2dtalk.innerText = "welcome!"
+})
