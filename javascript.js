628eeba9274b34e520a1e5cd2ebcd91e0818866c
yuanyinOuO <yuanyinOuO@gmail.com>
2022-07-28 16:24:26 +0800

    2022/7/28-4.24

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+/*
 function toggleMenu(number){
     var menu=document.getElementById("menu-"+number);
     menu.classList.toggle("hide");
@@ -21,3 +22,49 @@ note.addEventListener("mouseover" , function(){
 note.addEventListener("mouseout" , function(){
     live2dtalk.innerText = "welcome!"
 })
+*/
+/* 選單開合 */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+    } 
+    else {
+        dropdownContent.style.display = "block";
+    }
+    });
+}
+/* live2d talk */
+var live2dtalk = document.getElementById("live2d-talk"),
+    headimg = document.getElementById("headimg")
+    home = document.getElementById("home"),
+    note = document.getElementById("note")
+
+headimg.addEventListener("mouseover" , function(){
+    live2dtalk.innerText = "可愛的miku頭像OuO"
+})
+headimg.addEventListener("mouseout" , function(){
+    live2dtalk.innerText = "welcome my web!"
+})
+home.addEventListener("mouseover" , function(){
+    live2dtalk.innerText = "回到主頁面，雖然好像很沒用QQ"
+})
+home.addEventListener("mouseout" , function(){
+    live2dtalk.innerText = "welcome my web!"
+})
+note.addEventListener("mouseover" , function(){
+    live2dtalk.innerText = "可以前往我做的筆記，不過有些還沒整理好"
+})
+note.addEventListener("mouseout" , function(){
+    live2dtalk.innerText = "welcome my web!"
+})
+
+
+
+
+    
